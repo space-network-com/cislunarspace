8632eae296c81f76fd57878c7ce1370d89576346
space-network-com <spacenetworkcom@gmail.com>
2024-10-21 14:39:47 +0000

    Add products page

--- routes/jump.js
+++ routes/jump.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
 // Define a route for "/users"
 router.get('/about', (req, res) => {
-  res.send('This is the About page');
+ res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 router.get('/products', (req, res) => {
-  res.send('This is the Products page');
+   res.sendFile(path.join(__dirname, '../public/products.html'));
 });
 
 router.get('/demo', (req, res) => {
-  res.send('This is the demo page');
+  res.sendFile(path.join(__dirname, '../public/demo.html'));
 });
 
 router.get('/contact', (req, res) => {
-  res.send('This is the Contact page');
+  res.sendFile(path.join(__dirname, '../public/contact.html'));
 });
 
 
